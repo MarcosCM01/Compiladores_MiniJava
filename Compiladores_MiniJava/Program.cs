3d1edf66ddf1ebad4acb9ee2fe47b67efa55bbce
Jalbarizaez <jalbarizaez@gmail.com>
2020-08-17 23:08:02 -0600

    Creacion de buffer y lectura de archivo desde .exe

--- Compiladores_MiniJava/Program.cs
+++ Compiladores_MiniJava/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,32 @@ namespace Compiladores_MiniJava
     {
         static void Main(string[] args)
         {
+            const int bufferLenght = 5;
+            string URL = @"C:";//Direccion archivo de prueba
+            decimal Cantidad_Datos;
+            foreach (var arg in args)
+            {
+                using (var File = new FileStream(arg, FileMode.Open))
+                {
+                    var buffer = new byte[bufferLenght];
+                    using (var reader = new BinaryReader(File))
+                    {
+                        Cantidad_Datos = reader.BaseStream.Length;
+                        while (reader.BaseStream.Position != reader.BaseStream.Length)
+                        {
+                            buffer = reader.ReadBytes(bufferLenght);
+                            foreach(var item in buffer)
+                            {
+                                Console.Write(Convert.ToChar(item));
+                            }
+                            Console.WriteLine("");
+                        }
+                        
+                    }
+                    
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
