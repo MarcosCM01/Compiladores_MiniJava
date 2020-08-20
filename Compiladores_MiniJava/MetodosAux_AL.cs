f948109da0a0032e5de8046787e242a3a198936f
MarcosCM01 <40147926+MarcosCM01@users.noreply.github.com>
2020-08-19 18:39:45 -0600

    Creacion de metodos aux. Se inicia creaciones de ER para las constantes

---
+++ Compiladores_MiniJava/MetodosAux_AL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Compiladores_MiniJava
+{
+    public static class MetodosAux_AL
+    {
+        public static List<String> Reservadas = new List<string> 
+        {"void","int","double","boolean","string", "class", "const", "interface", "null", "this", "extends", "implements", "for", "while", "if", "else", "returns", "break", "New", "System", "out", "println" };
+
+        public static Dictionary<string, string> DiccionarioER_Valor = new Dictionary<string, string>() { { @"[a-zA-Z$]+[a-zA-Z0-9$]*$", "T_es_Id"}, {@"true|false", "T_es_ConstBool" }, {@"[0-9]+", "T_es_ConstDecimal"},
+        {@"0x[a-fA-F]" , ""} };
+
+        public static bool EsReservada(string palabra) 
+        {
+            Regex rx = new Regex(@"0x[a-fA-F]|[0-9]+");
+            if (Reservadas.Contains(palabra))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Compiladores_MiniJava/Program.cs
+++ Compiladores_MiniJava/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Compiladores_MiniJava
 {
@@ -11,32 +12,66 @@ namespace Compiladores_MiniJava
     {
         static void Main(string[] args)
         {
-            const int bufferLenght = 5;
-            string URL = @"C:";//Direccion archivo de prueba
+            const int bufferLenght = 50;
+            string URL = @"C:\Users\Marcos Andrés CM\Desktop\PRUEBA COMPI.txt";//Direccion archivo de prueba
             decimal Cantidad_Datos;
-            foreach (var arg in args)
-            {
-                using (var File = new FileStream(arg, FileMode.Open))
+            //foreach (var arg in args)
+            //{
+                using (var File = new FileStream(URL, FileMode.Open))
                 {
-                    var buffer = new byte[bufferLenght];
+                    var buffer = new char[bufferLenght];
                     using (var reader = new BinaryReader(File))
                     {
                         Cantidad_Datos = reader.BaseStream.Length;
-                        while (reader.BaseStream.Position != reader.BaseStream.Length)
+                        if (Cantidad_Datos > 0)//NO ESTÉ VACÍO EL ARCHIVO
                         {
-                            buffer = reader.ReadBytes(bufferLenght);
-                            foreach(var item in buffer)
+                            while (reader.BaseStream.Position != reader.BaseStream.Length)
                             {
-                                Console.Write(Convert.ToChar(item));
+                                buffer = reader.ReadChars(bufferLenght);
+                                var string_tmp = string.Empty;
+                                var flag_EspaciosBlancos = false;
+                                for (int posicion = 0; posicion < buffer.Length; posicion++)
+                                {
+                                    var item = buffer[posicion];
+                                    if (item != 9 && item != 10 && item != 13 && item != 32)//tab, /n, CR, espacio
+                                    {
+                                        string_tmp += item; //verificar que su longitud no sea mayor de 31
+                                    }
+                                    else
+                                    {
+                                       flag_EspaciosBlancos = true;
+                                    }
+                                    //ya analizo string_tmp
+                                    if (flag_EspaciosBlancos == true)
+                                    {
+                                        if (MetodosAux_AL.EsReservada(string_tmp) != false)
+                                        {//ES RESERVADA
+                                            string_tmp = string.Empty;
+                                        }
+                                        //APLICAR EXPRESIONES REGULARES PARA DETERMINAR LO QUE ES
+                                        else
+                                        {//VERIFICAMOS QUE SEA UN IDENTIFICADOR
+                                            Regex rx = new Regex(@"[a-zA-Z$]+[a-zA-Z0-9$]*$");
+                                            if (rx.IsMatch(string_tmp))
+                                            {//ES UN IDENTIFICADOR
+                                                var x = 0;
+                                            }
+                                        }
+                                        flag_EspaciosBlancos = false;
+                                    }
+                                }
+                                Console.WriteLine("");
                             }
-                            Console.WriteLine("");
                         }
-                        
+                        else
+                        {
+                            Console.WriteLine("ARCHIVO DE ENTRADA VACIO");
+                        }
                     }
                     
                 }
                 Console.ReadKey();
-            }
+            //}
         }
     }
 }
